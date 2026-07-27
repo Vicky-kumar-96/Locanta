60f906c91724e89a3a67d0fff6673393023ebe97
Vicky Kumar <vicky960885@gmail.com>
2026-07-27 22:59:42 +0530

    Update prject with new changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_BACKEND_URL + '/api'
 });
 
-// Attach the saved JWT (if any) to every outgoing request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
